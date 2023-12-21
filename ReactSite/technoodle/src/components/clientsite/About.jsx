7aeace53e606d75ca2dd96b53a41e37635f4794c
ryanmatthewrenaud <ryanmatthewrenaud@gmail.com>
2023-12-21 15:46:38 -0500

    Adjusted formatting and put a circular picture in the about section.

--- ReactSite/technoodle/src/components/clientsite/About.jsx
+++ ReactSite/technoodle/src/components/clientsite/About.jsx
@@ -1,10 +1,11 @@
-import pageicon from './assests/images/logo-main-nobg.png'
-
+import pageicon from './assests/images/logo-main-nobg-about.png'
+import clientsite from './assests/styling/clientsite.css'
 export default function About() {
     return (
         <div className="about-container">
             <div className="about-header">
                 <h3><b>About Us</b></h3>
+                <img className="about-icon" src={pageicon} />
                 <p className="about-text"><b>Lorem</b> ipsum,
                 </p>
                 <p className="about-text">
